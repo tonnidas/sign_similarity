e6f9a352b0789fa63a4557d60cc7f80b0b249af7
Tonni Das Jui <tonnijuicse@gmail.com>
2022-03-13 03:00:08 -0500

    Add matrices

--- process_matric_final.py
+++ process_matric_final.py
@@ -3,29 +3,36 @@ import nltk
 import os
 import pandas as pd
 
-def combineCounts(inputDF):
-    return
+def combineCounts(datasetName):
+    inputDF = pd.read_pickle(f'processed_matrices/matrices_{datasetName}')
+    inputDF = inputDF.drop('videoFileName', axis=1)
 
-# PSL---------------------------------------------------------------------------------------------------------------------------------------------
+    sumDF = inputDF.sum()
+    sumDF = pd.concat([pd.Series({"totalVideos": inputDF.shape[0]}), sumDF])
+    # sumDF["totalVideos"] = inputDF.shape[0]
 
-inputDF = pd.read_pickle('processed_matrices/matrices_PSL')
-cat = combineCounts(inputDF)
-cat.to_pickle('processed_matrices/matrices_PSL')
+    x = ["matchedWords", "matchedNoun", "matchedConj", "matchedAdj", "matchedVerbs", "matchedAdv", "matchedPoi", "matchedMisce", "matchedSentence"]
+    y = ["totalWords", "totalNoun", "totalConj", "totalAdj", "totalVerbs", "totalAdv", "totalPoi", "totalMisce", "totalVideos"]
 
-# ASL---------------------------------------------------------------------------------------------------------------------------------------------
+    for i in range(9):
+        matchedNoSyn = x[i] + "_noSyn"
+        matchedSyn = x[i] + "_Syn"
+        percentNoSyn = x[i] + "_noSyn_percent"
+        percentSyn = x[i] + "_Syn_percent"
 
-inputDF = pd.read_pickle('processed_matrices/matrices_ASL')
-cat = combineCounts(inputDF)
-cat.to_pickle('processed_matrices/matrices_ASL')
+        if(sumDF[y[i]] > 0):
+            sumDF[percentNoSyn] = round(sumDF[matchedNoSyn] * 100 / sumDF[y[i]], 2)
+            sumDF[percentSyn] = round(sumDF[matchedSyn] * 100 / sumDF[y[i]], 2)
+        else: # devide by zero
+            sumDF[percentNoSyn] = 100.0
+            sumDF[percentSyn] = 100.0
+    
+    return sumDF.to_frame(name = datasetName)
 
-# ISL---------------------------------------------------------------------------------------------------------------------------------------------
+sumDF = combineCounts('PSL')
+sumDF = sumDF.join(combineCounts('ASL'))
+sumDF = sumDF.join(combineCounts('ISL'))
+sumDF = sumDF.join(combineCounts('AUTSL'))
+print(sumDF)
 
-inputDF = pd.read_pickle('processed_matrices/matrices_ISL')
-cat = combineCounts(inputDF)
-cat.to_pickle('processed_matrices/matrices_ISL')
-
-# AUTSL---------------------------------------------------------------------------------------------------------------------------------------------
-
-inputDF = pd.read_pickle('processed_matrices/matrices_AUTSL')
-cat = combineCounts(inputDF)
-cat.to_pickle('processed_matrices/matrices_AUTSL')
+sumDF.to_csv('matrices_final.csv', sep=',', index=True, index_label='Matrices')
